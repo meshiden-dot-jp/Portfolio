66878d4611717b44173dc0cf156a641150576bde
meshiden-dot-jp <meganenasi@gmail.com>
2025-02-08 14:14:40 +0900

    update

---
+++ app/error.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+const error = () => {
+  return (
+    <div className="sm:w-[70%] w-[90%] m-auto py-24">
+            <h1 className='text-center text-9xl'>500</h1>
+            <h2 className='text-center text-4xl'>Page not found.</h2>
+            <p className='text-center text-lg pt-10'>申し訳ございません。<br />お探しのページは表示できませんでした。</p>
+
+            <div className="flex justify-center pt-10">
+                <Button asChild>
+                    <Link className="sm:w-1/4 w-full" href="/">ホームへ戻る</Link>
+                </Button>
+
+            </div>
+        </div>
+  )
+}
+
+export default error
