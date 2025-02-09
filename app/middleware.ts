ffbc7843952e8469c0c34728bfda6886e0b63ce9
meshiden-dot-jp <meganenasi@gmail.com>
2025-02-10 03:22:29 +0900

    update

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function middleware(_req: NextRequest) {
   const res = NextResponse.next();
 
   // HSTS ヘッダーを設定
