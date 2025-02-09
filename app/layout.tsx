9a36dabebf46a7d6b1cc6236cffdb7794f4c0158
meshiden-dot-jp <meganenasi@gmail.com>
2025-02-09 21:33:20 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,15 +29,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gaId = process.env.GA_ID || '';
   return (
     <html lang="ja">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/rvs7vvb.css"></link>
         <Script src="https://kit.fontawesome.com/4e6b2556d7.js"></Script>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+          strategy='afterInteractive'
+          async
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > 
+      >
         <Header />
         {children}
         <Footer />
