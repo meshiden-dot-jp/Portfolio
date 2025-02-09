535fa18762e58464428acd805169eb9e019e58db
meshiden-dot-jp <meganenasi@gmail.com>
2025-02-10 03:30:16 +0900

    update

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware() {
+export function middleware(req: NextRequest) {
   const res = NextResponse.next();
+  console.log(req.url);
 
   // HSTS ヘッダーを設定
   res.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
